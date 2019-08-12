b25d3b91998b8c8103a068be6c5b2197398b0a71
Adam Bethlehem <adam.bethlehem@derivco.co.za>
2019-08-12 18:54:04 +0200

    too good at this game bishes

--- src/main/java/com/array/index.java
+++ src/main/java/com/array/index.java
@@ -3,14 +3,23 @@ import com.array.ArrayChallenge;
 import com.array.*;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class index {
 
     public static void main(String[] args) {
 //        ArrayChallenge x = new ArrayChallenge();
 //        x.getNumber(5);
-        refTypeChallenge x = new refTypeChallenge();
-        Integer[] ar = x.readIntegers(5);
-        System.out.println(Array.get(ar,0));
+//        refTypeChallenge x = new refTypeChallenge();
+//        Integer[] ar = x.readIntegers(5);
+//        System.out.println(Array.get(ar,0));
+
+        reverseArray x = new reverseArray();
+        Integer[] a = x.arrayReverse();
+        for (int i =0; i <a.length; i ++){
+            System.out.println(a[i]);
+        }
+        String s = Arrays.toString(a);
+        System.out.println(s);
     }
 }
--- src/main/java/com/array/reverseArray.java
+++ src/main/java/com/array/reverseArray.java
@@ -1,14 +1,27 @@
 package com.array;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class reverseArray {
 
-    public Integer[] arrayReverse(int a){
-        Integer[] oArray = new Integer[a];
-        Collections.reverse(Arrays.asList(oArray));
-        return oArray;
+    public Integer[] arrayReverse(){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please reverse all, enter how many numbers to reverse");
+        int arraySize = s.nextInt();
+        s.nextLine();
+        Integer[] sampleArray = new Integer[arraySize];
+
+        for (int i =0; i <sampleArray.length; i++ ){
+            System.out.println("Enter " + i+1+ " number:");
+            int currentNumber = s.nextInt();
+            sampleArray[i] = currentNumber;
+            s.nextLine();
+        }
+        Collections.reverse(Arrays.asList(sampleArray));
+
+        return sampleArray;
     }
 }
